25459f07a2e96eb0f28dcfd5b03febcb72930987
Dmitriy Anisimkov <anisimko@adacore.com>
2020-10-16 17:17:19 +0600

    Detect error when input data to inflate is zero length
    TA13-028
    
    Do not ignore Z_BUF_ERROR if no data on input.
    
    Change-Id: I5741792e76e1ad115ff7f4b9c328b1074cebec57

--- zlib/gnatcoll-coders-zlib.adb
+++ zlib/gnatcoll-coders-zlib.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                             G N A T C O L L                              --
 --                                                                          --
---                       Copyright (C) 2019, AdaCore                        --
+--                     Copyright (C) 2019-2020, AdaCore                     --
 --                                                                          --
 -- This library is free software;  you can redistribute it and/or modify it --
 -- under terms of the  GNU General Public License  as published by the Free --
@@ -326,7 +326,8 @@ package body GNATCOLL.Coders.ZLib is
         and then
           (Code /= Z_BUF_ERROR
            or else Flush = No_Flush
-           or else In_Data'Length > 0)
+           or else In_Data'Length > 0
+           or else Total_In (Coder.Stream.all) = 0)
       then
          raise ZLib_Error with Return_Code_Enum'Image (Return_Code (Code)) &
            ": " & Last_Error_Message (Coder.Stream.all);
